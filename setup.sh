d617dfe366dd7080696a1779a2e4664ce7b0be63
fmash16 <fmash16@gmail.com>
2020-06-02 15:43:11 +0600

    edits

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Setup environment with i3,urxvt
 
-apt install i3 dmenu rxvt-unicode feh -y
+sudo apt install i3 dmenu rxvt-unicode feh -y
 mkdir ~/.config/i3
 cp ~/kali-setup-script/i3-config ~/.config/i3/config
 cp ~/kali-setup-script/Xresources ~/.Xresources
@@ -13,37 +13,37 @@ xrdb Xresources
 
 cd /tmp
 wget https://dl.google.com/go/go1.14.3.linux-amd64.tar.gz
-tar -C /usr/local -xvf go1.14.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.14.3.linux-amd64.tar.gz
 cd
 echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> ~/.bashrc
 source ~/.bashrc
-go get github.com/OJ/gobuster
-go get github.com/ffuf/ffuf
+sudo go get github.com/OJ/gobuster
+sudo go get github.com/ffuf/ffuf
 
 
 # Tools installation
 
-gem install evil-winrm
+sudo gem install evil-winrm
 ## Impacket tools 
   cd /tmp
   wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_9_21/impacket-0.9.21.tar.gz
   tar xzvf impacket-0.9.21.tar.gz
   cd impacket-0.9.21
-  pip install .
+  sudo pip install .
 
 
 # tools in /opt
 
 cd /opt
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
-git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
-wget https://download.sysinternals.com/files/PSTools.zip -P /tmp;unzip /tmp/PSTools.zip -d /opt/PSTools
+sudo git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
+sudo git clone https://github.com/danielmiessler/SecLists.git
+sudo git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
+sudo wget https://download.sysinternals.com/files/PSTools.zip -P /tmp;unzip /tmp/PSTools.zip -d /opt/PSTools
 ## Crackmapexec 
- apt-get install -y libssl-dev libffi-dev python-dev build-essential
- git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
+ sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential
+ sudo git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
  cd CrackMapExec
- python3 setup.py install
+ sudo python3 setup.py install
 
 
 
